442ebf68fdf488c49cd33b1d539865e7d55c5f10
Brage Andreas Trefjord <b.a.trefjord@fys.uio.no>
2025-03-01 19:57:08 +0100

    Updated

--- Oblig1/oblig1.py
+++ Oblig1/oblig1.py
@@ -52,17 +52,44 @@ for i in range(num_points):
     nz = n-nx-ny
     Fi = find_F(nx, ny, nz)
     eq_ind = np.argmin(Fi)
+    F_eq = Fi.flatten()[eq_ind]
+    F_vals[i] = F_eq
+    
     nx_eq = nx.flatten()[eq_ind]
     ny_eq = ny.flatten()[eq_ind]
     nz_eq = nz.flatten()[eq_ind]
-    F_eq = Fi.flatten()[eq_ind]
-    
-    F_vals[i] = F_eq
-    nx_vals[i] = nx_eq
-    ny_vals[i] = ny_eq
-    nz_vals[i] = nz_eq
+
+    ni_vals[0][i] = nx_eq
+    ni_vals[1][i] = ny_eq
+    ni_vals[2][i] = nz_eq
+    # nx_vals[i] = nx_eq
+    # ny_vals[i] = ny_eq
+    # nz_vals[i] = nz_eq
 
 # ddF = np.gradient(np.gradient(F))
+
+# Locate phase transition from instabillity
+ddF_n = np.gradient(np.gradient(F_vals))
+trans_ind = np.argwhere(ddF_n <= 0)
+trans_start_ind = trans_ind[0]
+trans_end_ind = trans_ind[-1]
+
+n_trans_start = n_vals[trans_start_ind]
+n_trans_end = n_vals[trans_end_ind]
+pre_trans_ind = np.arange(trans_start_ind[0])
+post_trans_ind = np.arange(trans_end_ind[0], num_points)
+
+# Values before phase transition
+n_pre = n_vals[pre_trans_ind]
+nx_pre = nx_vals[pre_trans_ind]
+ny_pre = ny_vals[pre_trans_ind]
+nz_pre = nz_vals[pre_trans_ind]
+
+# Values after phase transition
+n_post = n_vals[post_trans_ind]
+nx_post = nx_vals[post_trans_ind]
+ny_post = ny_vals[post_trans_ind]
+nz_post = nz_vals[post_trans_ind]
 
 plt.figure()
 plt.title("$F(n)$ at equilibrium")
@@ -76,37 +103,92 @@ plt.figure()
 plt.title("Second derivative of $F(n)$ at equilibrium")
 plt.xlabel("$n=N/V$")
 plt.ylabel("$\\left(\\frac{d^2F}{dn^2}\\right)_{V,T}$")
-plt.plot(n_vals, np.gradient(np.gradient(F_vals)))
+plt.plot(n_vals, ddF_n)
 plt.axhline(0, linestyle="dashed", color="black")
-plt.axvline(0.29, linestyle="dashed", color="black")
-plt.axvline(0.32, linestyle="dashed", color="black")
+plt.axvline(n_trans_start, linestyle="dashed", color="black")
+plt.axvline(n_trans_end, linestyle="dashed", color="black")
 plt.savefig("ddF_n.pdf")
 
 plt.figure()
 plt.title("Number of each rod $n_i$ at equilibrium")
 plt.xlabel("$n$")
 plt.ylabel("$n_i$")
-# plt.plot(n_vals, nx_vals, linestyle="solid", color="blue", label="$n_x$")
-# plt.plot(n_vals, ny_vals, linestyle="dashed", color="red", label="$n_y$")
+plt.plot(n_vals, nx_vals, linestyle="solid", color="blue", label="$n_x$")
+plt.plot(n_vals, ny_vals, linestyle="dashed", color="red", label="$n_y$")
 plt.plot(n_vals, nz_vals, linestyle="solid", color="green", label="$n_z$")
-plt.axvline(0.29, linestyle="dashed", color="black")
-plt.axvline(0.32, linestyle="dashed", color="black")
+plt.axvline(n_trans_start, linestyle="dashed", color="black")
+plt.axvline(n_trans_end, linestyle="dashed", color="black")
 plt.legend()
 plt.savefig("ni_n.pdf")
 
-
-pre_trans_ind = np.where(n_vals <= 0.29)
-n_pre = n_vals(pre_trans_ind)
-nx_pre = nx_vals(pre_trans_ind)
-ny_pre = ny_vals(pre_trans_ind)
-nz_pre = nz_vals(pre_trans_ind)
-
-post_trans_ind = np.where(n_vals >= 0.32)
-n_post = n_vals(post_trans_ind)
-nx_post = nx_vals(post_trans_ind)
-ny_post = ny_vals(post_trans_ind)
-nz_post = nz_vals(post_trans_ind)
+# nz_sort_ind = np.argsort(nz_vals)
+# dF_dnz_sort = np.gradient(F_vals[nz_sort_ind], nz_vals[nz_sort_ind])
+# plt.figure()
+# plt.title("Derivative of $F(n_z)$")
+# plt.xlabel("$n_z$")
+# plt.ylabel("$\\frac{dF}{dn_z}$")
+# plt.plot(n_vals[nz_sort_ind], dF_dnz_sort, linestyle="solid", color="blue", label="$\\frac{dF}{dn_z}$")
+# # plt.plot(n_vals, ny_vals, linestyle="dashed", color="red", label="$n_y$")
+# # plt.plot(n_vals, nz_vals, linestyle="solid", color="green", label="$n_z$")
+# plt.axvline(n_trans_start, linestyle="dashed", color="black")
+# plt.axvline(n_trans_end, linestyle="dashed", color="black")
+# plt.legend()
+# plt.savefig("dF_nz.pdf")
 
 nx_pre_mean = np.mean(nx_pre)
 ny_pre_mean = np.mean(ny_pre)
 nz_pre_mean = np.mean(nz_pre)
+
+
+def mui(nx, ny, nz, i):
+    ni = [nx, ny, nz]
+    ans = 1
+    for j in range(3):
+        if j == i:
+            ans += np.log(alpha * ni[j])
+        else:
+            ans += gamma * ni[j]
+    return T * ans
+
+mux = mui(nx_vals, ny_vals, nz_vals, 0)
+muy = mui(nx_vals, ny_vals, nz_vals, 1)
+muz = mui(nx_vals, ny_vals, nz_vals, 2)
+
+plt.figure()
+plt.title("Chemical potential of each rod orientation")
+plt.xlabel("$n$")
+plt.ylabel("$\\mu_i$")
+plt.plot(n_vals, mux, linestyle="solid", color="blue", label="$\\mu_x$")
+plt.plot(n_vals, muy, linestyle="dashed", color="red", label="$\\mu_y$")
+plt.plot(n_vals, muz, linestyle="solid", color="green", label="$\\mu_z$")
+plt.axvline(n_trans_start, linestyle="dashed", color="black")
+plt.axvline(n_trans_end, linestyle="dashed", color="black")
+plt.legend()
+plt.savefig("mui_n.pdf")
+
+def find_P(nx, ny, nz):
+    return T * (nx + ny + nz + gamma*(nx*ny + ny*nz + nz*nx))
+
+P_vals = find_P(nx_vals, ny_vals, nz_vals)
+
+plt.figure()
+plt.title("Pressure at equilibrium")
+plt.xlabel("$n$")
+plt.ylabel("$P$")
+plt.plot(n_vals, P_vals, linestyle="solid", color="blue", label="$P$")
+plt.axvline(n_trans_start, linestyle="dashed", color="black")
+plt.axvline(n_trans_end, linestyle="dashed", color="black")
+plt.legend()
+plt.savefig("P_n.pdf")
+
+plt.figure()
+plt.title("Chemical potential of each rod orientation")
+plt.xlabel("$\\mu_i$")
+plt.ylabel("$n$")
+plt.plot(mux, n_vals, linestyle="solid", color="blue", label="$\\mu_x$")
+plt.plot(muy, n_vals, linestyle="dashed", color="red", label="$\\mu_y$")
+plt.plot(muz, n_vals, linestyle="solid", color="green", label="$\\mu_z$")
+plt.axhline(n_trans_start, linestyle="dashed", color="black")
+plt.axhline(n_trans_end, linestyle="dashed", color="black")
+plt.legend()
+plt.savefig("n_mui.pdf")
