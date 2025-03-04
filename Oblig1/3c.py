72fa37ebcaa7ab8194892da052ee2602feea6cd2
Brage Andreas Trefjord <b.a.trefjord@fys.uio.no>
2025-03-04 18:08:10 +0100

    Finally got some interesting results in 3c

--- Oblig1/3c.py
+++ Oblig1/3c.py
@@ -1,12 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-## Set constants
 alpha = 1
 gamma = 10
 T = 1
-N = 1
-
+N = 1000
 
 ## Function for finding F
 def find_F(Nx, Ny, Nz, V):
@@ -18,45 +16,36 @@ def find_F(Nx, Ny, Nz, V):
         Nii = Nvals[(i+1) % 3]
         Niii = Nvals[(i+2) % 3]
         Nilog += Ni*np.log(alpha*Ni/V)
-        cross_terms += Nii*Niii
-    ans = T * (Nilog + gamma * cross_terms/V)
+        cross_terms += Nii*Niii/V
+    ans = T * (Nilog + gamma * cross_terms)
     return ans
 
-## Define region of P and N to consider, and create arrays
+Plow = 0.5
+Phigh = 5
 num_points = 1000
-Plow = 0.4
-Phigh = 0.7
+
 P_vals = np.linspace(Plow, Phigh, num_points)
 V_vals = np.zeros(num_points)
 F_vals = np.zeros(num_points)
-G_vals = np.zeros(num_points)
-
-Nx_vals = np.zeros(num_points)
-Ny_vals = np.zeros(num_points)
-Nz_vals = np.zeros(num_points)
-Ni_vals = [Nx_vals, Ny_vals, Nz_vals]
+Nx_values = np.zeros(num_points)
+Ny_values = np.zeros(num_points)
+Nz_values = np.zeros(num_points)
+Ni_vals = [Nx_values, Ny_values, Nz_values]
 
 grid_size = 100
-
 for i in range(num_points):
     P = P_vals[i]
-    Nxy_range = np.linspace(N/100, N/10, grid_size)
+    Nxy_range = np.linspace(N/1e10, N/3, grid_size)
     Nx, Ny = np.meshgrid(Nxy_range, Nxy_range)
     Nz = N-Nx-Ny
     Nxyz = Nx*Ny + Ny*Nz + Nz*Nx
-    V = T*N/(2*P) * np.sqrt(1 + 4*gamma*P/T * Nxyz/(N*N))
-    Fi = find_F(Nx, Ny, Nz, V)
-    Gi = Fi + P*V
+
+    V = T*N/(2*P) * (1 + np.sqrt(1 + 4*P*gamma*Nxyz/(T*N*N)))
     
-    eq_ind = np.argmin(Gi)
-
-    G_eq = Gi.flatten()[eq_ind]
-    G_vals[i] = G_eq
-
+    Fi = find_F(Nx, Ny, Nz, V)
+    eq_ind = np.argmin(Fi)
     F_eq = Fi.flatten()[eq_ind]
     F_vals[i] = F_eq
-
-    V_vals[i] = V.flatten()[eq_ind]
 
     Nx_eq = Nx.flatten()[eq_ind]
     Ny_eq = Ny.flatten()[eq_ind]
@@ -65,18 +54,12 @@ for i in range(num_points):
     Ni_vals[0][i] = Nx_eq
     Ni_vals[1][i] = Ny_eq
     Ni_vals[2][i] = Nz_eq
+    
+    V_eq = V.flatten()[eq_ind]
+    V_vals[i] = V_eq
 
-mu_vals = G_vals/N
 
-## Make plots
+
 plt.figure()
-plt.title("$G(P)$ at equilibrium")
-plt.xlabel("$P$")
-plt.ylabel("$G$")
-# plt.plot(V_vals, P_vals)
-# plt.plot(V_vals, F_vals)
-# plt.plot(V_vals, G_vals)
-plt.plot(V_vals, np.gradient(np.gradient(G_vals)))
-# plt.axvline(n_trans_start, linestyle="dashed", color="black")
-# plt.axvline(n_trans_end, linestyle="dashed", color="black")
-plt.savefig("Figures_3c/G_n.pdf")
+plt.plot(P_vals, V_vals)
+plt.savefig("PV.pdf")
