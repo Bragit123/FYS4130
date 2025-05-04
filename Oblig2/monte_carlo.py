56f0c71c2f42f2786c0f71917e49b108d2ba0234
Brage Andreas Trefjord <b.a.trefjord@fys.uio.no>
2025-05-04 17:35:59 +0200

    Added MC simulation as a class to simplify running different parameters

--- Oblig2/monte_carlo.py
+++ Oblig2/monte_carlo.py
@@ -6,128 +6,138 @@ import matplotlib.pyplot as plt
 
 np.random.seed(100)
 
-## Dimension of system. Should be 1 or 2 (1D chain or 2D lattice)
+
+class Spin_Model:
+    def __init__(self, d, L, T):
+        self.d = d # Dimension of system. Should be 1 or 2 (1D chain or 2D lattice)
+        self.L = L # System size
+        self.T = T # Temperature in units of J
+
+        self.q = 3 # Number of spin states
+
+        ## Define index and position arrays depending on system dimension
+        if d == 1:
+            self.N = L
+            self.indices = np.arange(self.N).reshape(self.N, 1)
+            self.x_arr = self.indices[:,0]
+            self.y_arr = np.zeros(self.N, dtype=int)
+        elif d == 2:
+            self.N = L*L
+            self.indices = np.arange(self.N).reshape(L, L)
+            y_arr, x_arr = np.meshgrid(np.arange(L), np.arange(L))
+            self.x_arr = x_arr.flatten()
+            self.y_arr = y_arr.flatten()
+        
+        self.S = np.zeros(self.N, dtype=int) # Array containing spin state of each site
+        self.P_connect = 1 - np.exp(-1/T) # Connection probability
+
+        # Possible values of m_j from spin
+        self.m_spin = np.array([
+                1,
+                np.exp(2*np.pi/3 * 1j),
+                np.exp(4*np.pi/3 * 1j)
+        ])
+
+        # Initialize magnetization counter
+        self.M_count = np.zeros(self.q)
+        self.M_count[0] = self.N
+    
+    def neighbor_index(self, i, dir):
+        # Find position from index
+        x = self.x_arr[i]
+        y = self.y_arr[i]
+
+        if dir == 0:
+            # Right
+            return self.indices[(x+1)%self.L, y]
+        elif dir == 1:
+            # Up
+            return self.indices[x, (y+1)%self.L]
+        elif dir == 2:
+            # Left
+            return self.indices[(x-1+self.L)%self.L, y]
+        elif dir == 3:
+            # Down
+            return self.indices[x, (y-1+self.L)%self.L]
+        else:
+            raise ValueError("dir must be 0, 1, 2 or 3 (right, up, left, down)")
+
+    def flip_and_build(self, i):
+        # Run one cluster starting from i by going through neighbours recursively
+        old_state = self.S[i]
+        new_state = (self.S[i]+1)%self.q
+
+        self.S[i] = new_state # Flip the spin
+        
+        # Count spins
+        self.M_count[old_state] -= 1
+        self.M_count[new_state] += 1
+
+        if self.d == 1:
+            dir_arr = [0, 2] # Only right and left for 1D systems
+        else:
+            dir_arr = [0, 1, 2, 3] # All 4 directions for 2D systems
+
+        for dir in dir_arr:
+            j = self.neighbor_index(i, dir)
+            if (self.S[j] == old_state):
+                if (np.random.uniform() < self.P_connect):
+                    self.flip_and_build(j)
+    
+    ## Analytic correlation function
+    def corr_func_anal(self, r):
+        lam0 = np.exp(1/self.T) + 2
+        lam1 = np.exp(1/self.T) - 1
+        
+        num = lam1**self.N + lam0**r * lam1**(self.N-r) + lam0**(self.N-r) * lam1**r
+        den = lam0**self.N + 2 * lam1**self.N
+
+        return num/den
+
+    def run_MC(self, n_clusters=5, n_steps_eq=10000, n_steps=10000):
+        ## Equilibrate
+        for t in range(n_steps_eq):
+            for c in range(n_clusters):
+                self.flip_and_build(np.random.randint(0, self.N))
+        
+        ## Run measurements
+        mit = np.zeros((self.N, n_steps), dtype=complex)
+
+        for t in range(n_steps):
+            for c in range(n_clusters):
+                self.flip_and_build(np.random.randint(0, self.N))
+            
+            mit[:, t] = self.m_spin[self.S]
+
+        m0t_mit = np.conj(mit[0,:]) * mit
+        mi_mean = np.mean(mit, axis=1)
+
+        corr_func = np.mean(m0t_mit, axis=1) - np.conj(mi_mean[0]) * mi_mean
+
+        return corr_func
+
+
+#### Problem 2b)
+
+## Set parameters
 d = 1
+L = 16
+T_vals = [0.25, 0.5]
+T_text = ["T_0_25", "T_0_5"]
 
-q = 3 # q spin states
-L = 16 # System size
-T = 0.5 # Temperature in units of J
-P_connect = 1 - np.exp(-1/T) # Connection probability
+for i in range(len(T_vals)):
+    T = T_vals[i]
+    model = Spin_Model(d, L, T)
+    r_vals = np.arange(0, model.N)
+    corr_anal = model.corr_func_anal(r_vals) # Find analytic correlation function
+    corr_MC = model.run_MC() # Run MC simulation
 
-## Number of MC steps and Wolff clusters
-n_clusters = 5 # Number of clusters per MC step
-n_steps_eq = 10000 # Number of equilibrium MC steps
-n_steps = 10000 # Number of measurement MC steps
-
-## Define index and position arrays depending on system dimension
-if d == 1:
-    N = L
-    indices = np.arange(N).reshape(N, 1)
-    x_arr = indices[:,0]
-    y_arr = np.zeros(N, dtype=int)
-
-if d == 2:
-    N = L*L
-    indices = np.arange(N).reshape(L, L)
-    y_arr, x_arr = np.meshgrid(np.arange(L), np.arange(L))
-    x_arr = x_arr.flatten()
-    y_arr = y_arr.flatten()
-
-
-S = np.zeros(N, dtype=int) # Array containing spin state of each site
-
-# Possible values of m_j from spin
-m_spin = np.array([
-        1,
-        np.exp(2*np.pi/3 * 1j),
-        np.exp(4*np.pi/3 * 1j)
-])
-
-# Initialize magnetization counter
-M_count = np.zeros(q)
-M_count[0] = N
-
-def neighbor_index(i, dir):
-    # Find position from index
-    x = x_arr[i]
-    y = y_arr[i]
-
-    if dir == 0:
-        # Right
-        return indices[(x+1)%L, y]
-    elif dir == 1:
-        # Up
-        return indices[x, (y+1)%L]
-    elif dir == 2:
-        # Left
-        return indices[(x-1+L)%L, y]
-    elif dir == 3:
-        # Down
-        return indices[x, (y-1+L)%L]
-    else:
-        raise ValueError("dir must be 0, 1, 2 or 3 (right, up, left, down)")
-
-
-def flip_and_build(i):
-    # Run one cluster starting from i by going through neighbours recursively
-    old_state = S[i]
-    new_state = (S[i]+1)%q
-
-    S[i] = new_state # Flip the spin
-    
-    # Count spins
-    M_count[old_state] -= 1
-    M_count[new_state] += 1
-
-    if d == 1:
-        dir_arr = [0, 2] # Only right and left for 1D systems
-    else:
-        dir_arr = [0, 1, 2, 3] # All 4 directions for 2D systems
-
-    for dir in dir_arr:
-        j = neighbor_index(i, dir)
-        if (S[j] == old_state):
-            if (np.random.uniform() < P_connect):
-                flip_and_build(j)
-
-
-## Equilibrate
-for t in range(n_steps_eq):
-    for c in range(n_clusters):
-        flip_and_build(np.random.randint(0, N))
-
-## Run measurements
-mit = np.zeros((N, n_steps), dtype=complex)
-
-for t in range(n_steps):
-    for c in range(n_clusters):
-        flip_and_build(np.random.randint(0,N))
-    
-    mit[:, t] = m_spin[S]
-
-m0t_mit = np.conj(mit[0,:]) * mit
-mi_mean = np.mean(mit, axis=1)
-
-corr_func = np.mean(m0t_mit, axis=1) - np.conj(mi_mean[0]) * mi_mean
-
-
-## Analytic correlation function
-def corr_func_anal(r):
-    lam0 = np.exp(1/T) + 2
-    lam1 = np.exp(1/T) - 1
-    
-    num = lam1**N + lam0**r * lam1**(N-r) + lam0**(N-r) * lam1**r
-    den = lam0**N + 2 * lam1**N
-
-    return num/den
-
-## Plot
-r_vals = np.arange(0, N)
-corr_anal = corr_func_anal(r_vals)
-
-plt.xlabel("$r$")
-plt.ylabel("$C(r)$")
-plt.plot(r_vals, corr_anal, label="Analytic")
-plt.plot(r_vals, np.real(corr_func), label="Real")
-plt.legend()
-plt.savefig("corr_func.pdf")
+    ## Plot results
+    plt.figure()
+    plt.title(f"Correlation function for 1D spin chain with temperature $T={T_vals[i]}$J")
+    plt.xlabel("$r$")
+    plt.ylabel("$C(r)$")
+    plt.plot(r_vals, corr_anal, "k", label="Analytic")
+    plt.plot(r_vals, np.real(corr_MC), "or", label="MC simulation")
+    plt.legend()
+    plt.savefig(f"corr_func_1D_{T_text[i]}.pdf")
